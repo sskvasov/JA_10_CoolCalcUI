5f08c94a97920bfd4a8d4c9d8d6859ad7b6a4504
Квасов Сергей Сергеевич <sskvasov@dev.vtb.ru>
2021-08-16 10:54:11 +0300

    первый

--- src/main/java/my/supercalc/CalcFrame.java
+++ src/main/java/my/supercalc/CalcFrame.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CalcFrame extends JFrame {
+
     JTextField text1 = new JTextField();
     JTextField text2 = new JTextField();
     JTextField text3 = new JTextField();
@@ -11,14 +12,19 @@ public class CalcFrame extends JFrame {
     JProgressBar bar = new JProgressBar();
 
     public CalcFrame() {
-        this.setBounds(5, 5, 500, 500);
         this.setLayout(null);
-        text1.setBounds(5, 5, 90, 20);
-        text2.setBounds(5, 25, 90, 20);
-        button.setBounds(5, 50, 90, 40);
+        this.setBounds(20,20 ,145, 190);
+        text1.setBounds(5,5,120,20);
+        text2.setBounds(5,30,120,20);
+        button.setBounds(5,55,120,40);
         button.setText("+");
-        text3.setBounds(5, 95, 90, 20);
-        bar.setBounds(5, 120, 90, 15);
+        text3.setBounds(5,100,120,20);
+        bar.setBounds(5,125,120,15);
+
+        text1.setHorizontalAlignment(JTextField.CENTER);
+        text2.setHorizontalAlignment(JTextField.CENTER);
+        text3.setHorizontalAlignment(JTextField.CENTER);
+
         this.add(text1);
         this.add(text2);
         this.add(text3);
@@ -26,30 +32,34 @@ public class CalcFrame extends JFrame {
         this.add(bar);
 
         button.addActionListener(e -> {
+            text3.setText("Wait...");
             int x = Integer.parseInt(text1.getText());
             int y = Integer.parseInt(text2.getText());
             SlowCalc slow = new SlowCalc();
-            Thread thread = new Thread(() -> {
-                //Так просто, но так нельзя
+            Thread thread = new Thread( () -> {
+                //Так просто, но так нелья:
                 //UI элементы можно обновлять только
-                //из потока, в котором они созданы
-//                slow.addProgressListener(percent -> {
+                // из потока, в котором они созданы
+//                slow.addProgressListener( percent -> {
 //                    bar.setValue(percent);
 //                });
 //                int z = slow.plus(x, y);
 //                text3.setText(String.valueOf(z));
-                slow.addProgressListener(percent -> {
+
+                //Так длинее, но зато проавильно:
+                slow.addProgressListener( percent -> {
                     EventQueue.invokeLater(() -> {
                         bar.setValue(percent);
+                    });
                 });
                 int z = slow.plus(x, y);
                 EventQueue.invokeLater(() -> {
                     text3.setText(String.valueOf(z));
                 });
+
             });
             thread.start();
-
-        }
+        });
     }
 
     public static void main(String[] args) {
--- src/main/java/my/supercalc/SlowCalc.java
+++ src/main/java/my/supercalc/SlowCalc.java
@@ -1,6 +1,7 @@
 package my.supercalc;
 
 public class SlowCalc {
+
     ProgressListener listener;
 
     public void addProgressListener(ProgressListener listener) {
@@ -8,17 +9,18 @@ public class SlowCalc {
     }
 
     public int plus(int x, int y) {
-        for (int i = 1; i <= 100; i++) {
+        for(int i=1; i<=100; i++ ) {
             try {
                 Thread.sleep(200);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            if(listener!=null){
+            if (listener != null) {
                 listener.progress(i);
             }
         }
         return x + y;
     }
+
 }
 
